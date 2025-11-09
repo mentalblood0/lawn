2b9900d375436a4ce633a5c21cfc3e9b3ef52ba6
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-11-09 16:29:08 +0300

    more accurate splitting approach: add middles instead of filling the whole gap

--- src/variable_data_pool.rs
+++ src/variable_data_pool.rs
@@ -3,42 +3,42 @@ use std::path::PathBuf;
 
 pub fn split_scale_logarithmically(max_value: usize) -> Result<Vec<usize>, String> {
     const VALUES_COUNT: usize = 256;
-
     if max_value < VALUES_COUNT {
         return Err(format!(
             "Can not split scale: maximum value {max_value} must be greater or equal to values count {VALUES_COUNT}"
         ));
-    } else {
-        let mut result: Vec<usize> = Vec::with_capacity(VALUES_COUNT);
-
-        for current_values_count in 0..(VALUES_COUNT - 3) {
-            let new_value = (2 as f64).powf(
-                (max_value as f64).log2() * current_values_count as f64 / (VALUES_COUNT - 1) as f64,
-            ) as usize;
-            if result.len() == 0 || *result.last().unwrap() != new_value {
-                result.push(new_value);
-            }
-        }
-
-        let additional_values_count = VALUES_COUNT - 1 - result.len();
-        if additional_values_count > 0 {
-            let mut additional_values: Vec<usize> = Vec::with_capacity(additional_values_count);
-            'result_two_values_loop: for two_values in result.windows(2) {
-                for new_value in two_values[0] + 1..two_values[1] {
-                    additional_values.push(new_value);
-                    if additional_values.len() == additional_values_count {
-                        break 'result_two_values_loop;
-                    }
-                }
-            }
-            result.append(&mut additional_values);
-        }
-
-        result.sort();
-        result.push(max_value);
-
-        Ok(result)
     }
+
+    let mut result: Vec<usize> = Vec::with_capacity(VALUES_COUNT);
+
+    for current_values_count in 0..(VALUES_COUNT - 3) {
+        let new_value = (2 as f64).powf(
+            (max_value as f64).log2() * current_values_count as f64 / (VALUES_COUNT - 1) as f64,
+        ) as usize;
+        if result.len() == 0 || *result.last().unwrap() != new_value {
+            result.push(new_value);
+        }
+    }
+
+    let additional_values_count = VALUES_COUNT - 1 - result.len();
+    if additional_values_count > 0 {
+        let mut additional_values: Vec<usize> = Vec::with_capacity(additional_values_count);
+        for two_values in result.windows(2) {
+            if two_values[1] - two_values[0] == 1 {
+                continue;
+            }
+            additional_values.push((two_values[0] + two_values[1]) / 2);
+            if additional_values.len() == additional_values_count {
+                break;
+            }
+        }
+        result.append(&mut additional_values);
+    }
+
+    result.sort();
+    result.push(max_value);
+
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -50,8 +50,6 @@ mod tests {
         let max_value = 65536;
 
         let result = split_scale_logarithmically(max_value).unwrap();
-        // dbg!(&result);
-        // assert!(false);
         assert!(result.is_sorted());
         assert_eq!(result.len(), 256);
         assert_eq!(*result.last().unwrap(), max_value);
